04991646baf309c257ed9cddf0b15318edc54c9b
Marcelo Marcon <mmarconm@users.noreply.github.com>
2020-07-30 21:49:06 -0400

    Update hello_world.asm

--- amd64/part_one/hello_world.asm
+++ amd64/part_one/hello_world.asm
@@ -1,8 +1,20 @@
-	section .data
-msg:	db "Hello World", 10
-msg_L:	 equ $-msg
+	;; nasm -f elf64 hello.s
+	;; ld -s -o hello hello.o
+	;; sudo apt install nasm fasm
+	
+	;; SYS_EXIT        equ 1
+	;; RET_EXIT        equ 5
+	;; SYS_READ        equ 3
+	;; SYS_WRITE       equ 4
+	;; STD_IN          equ 0
+	;; STD_OUT         equ 1
+	;; MAX_IN          equ 10
 
-	section .text
+section .data
+    msg:	db "Hello World", 10
+    msg_L:	 equ $-msg
+
+section .text
 	global _start
 
 _start:
